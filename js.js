97d2c881847ff097c83523f750c980e368913a04
Kevin Kawan <104184907+K4lbers@users.noreply.github.com>
2023-01-09 22:03:33 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+aplicar.addEventListener('click', () => {
+    menu.classList.toggle('aparecer');
+    home.classList.toggle('aparecer');
+    historia.classList.toggle('aparecer');
+    experiencia.classList.toggle('aparecer');
+    extracurriculares.classList.toggle('aparecer')
+})
